7eca4c4294d878eafc47a8d9b9e87cac9fa827fc
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-24 22:59:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var path = require('path');
 /*var Pool = require('pg').Pool;*/
 var app = express();
 
-var config = {
+/*var config = {
     user:'iamnishantrai',
     database:'iamnishantrai',
     host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB.PASSWORD
-};
+};*/
 
 app.use(morgan('combined'));
 
