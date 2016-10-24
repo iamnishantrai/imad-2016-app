f98bb5256e6dcb8f14bae7109ab435b340237851
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-24 22:58:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
+/*var Pool = require('pg').Pool;*/
 var app = express();
 
 var config = {
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config);
+/*var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test', function (err,result) {
         if(err){
@@ -28,7 +28,7 @@ app.get('/test-db',function(req,res){
             res.send(JSON.stringify(result));
         }
     });
-});
+});*/
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
