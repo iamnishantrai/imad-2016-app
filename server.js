fe7cc03f7e9f9a4b36831e6a806017f06b48d712
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-24 22:51:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ var config = {
     database:'iamnishantrai',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB.PASSWORD,
-    
+    password:process.env.DB.PASSWORD
 };
 
 var app = express();
