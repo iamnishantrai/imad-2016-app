d5c0d486aa2dd4457588d734214f800471d5c33c
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-24 22:56:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var Pool = require('pg').Pool;
 var config = {
     user:'iamnishantrai',
     database:'iamnishantrai',
-    host:'db.imad.hasura-app.io',
+    host:'db.imad.hasura-ap p.io',
     port:'5432',
     password:process.env.DB.PASSWORD
 };
@@ -14,11 +14,12 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var pool = new Pool(config);
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
-    //
-    //
     pool.query('SELECT * FROM test', function (err,result) {
         if(err){
             res.status(500).send(err.toString());
@@ -27,10 +28,6 @@ app.get('/test-db',function(req,res){
             res.send(JSON.stringify(result));
         }
     });
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/main.js', function (req, res) {
