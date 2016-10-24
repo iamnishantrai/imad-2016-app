096cceac1a2e49a5889e940dec9e1c629cb09637
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-24 22:33:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //
     //
-    pool.query('SELECT * FROM test', funtion(err,result)){
+    pool.query('SELECT * FROM test', function (err,result) {
         if(err){
             res.status(500).send(err.toString());
         }
         else{
             res.send(JSON.stringify(result));
         }
-    }
+    });
 });
 
 app.get('/', function (req, res) {
