8fc3b23e4139bbc384e9e062b8d2f1e7acd246b0
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-24 19:29:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
