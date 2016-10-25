22227b464e0ccdda1b59d2df0f645697b6283660
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 22:41:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/test-db',function(req,res){
             res.status(500).send(err.toString());
         }
         else{
-            res.send(JSON.stringify(result.rows) +"\n"+ JSON.stringify(result));
+            res.send(JSON.stringify(result.rows) +"<br><hr>"+ JSON.stringify(result));
         }
     });
 });
