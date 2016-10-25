7dcd62c7f0a4e3e19a2d76284445f3cb29b2ea9f
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 23:11:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.get('/submitname', function(req,res){
 app.get('/articles/:articleName',function(req,res){
     
     pool.query("SELECT * FROM article WHERE title="+req.params.articleName, function(err,result){
-        iff(err){
+        if(err){
             res.status(500).send(err.toString());
         } else {
             if(result.rows.length === 0){
