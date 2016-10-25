e2b4d32742497ce2036cc219de6619f12fe24d3c
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 19:25:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,32 @@
-var counter = 0;
+//var counter = 0;
 
 function onClick(){
 
-    counter = counter +1;
-    document.getElementById("count").innerHTML = counter.toString();
+    //counter = counter +1;
+    //document.getElementById("count").innerHTML = counter.toString();
     
     
-    /*var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE){
+            if(request.status==200){
+                var counter = request.responseText;
+                document.getElementById("count").innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    
+    
+    
+    /*request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var counter = xhhtp.reponseText;
             document.getElementById("count").innerHTML = counter;
         }
-    };
+    };*/
     
     request.open("GET", "http://iamnishantrai.imad.hasura-app.io/counter", true);
-    request.send();*/
+    request.send(null);
 }
