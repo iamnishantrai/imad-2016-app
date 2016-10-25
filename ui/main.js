8ecf6b7a9e211037d7e66914fefda2928e53874a
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 19:03:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,4 @@
-var button = document.getElementById("counter");
-
 var counter = 0;
-
-button.onClick = function(){
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    a.innerHTML = counter;
-};
-
 
 function onClick(){
     counter = counter + 1;
