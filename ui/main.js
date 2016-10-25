161d51f39b3b1a4464d49372115b6e5fc82b0fef
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 19:13:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,10 @@
-var counter = 0;
-
 function onClick(){
-    counter = counter + 1;
-    document.getElementById("count").innerHTML = counter;
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var counter = xhhtp.reponseText;
+                document.getElementById("count").innerHTML = counter;
+        }
+    };
 }
