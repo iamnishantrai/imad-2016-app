b039ba8d62de8ec1deca02d9b64e80e3f65704a7
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 20:21:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,14 +124,14 @@ app.get('/:articleName',function(req,res){
 
 var names = [];
 
-app.get('/submitname/:name', function(req,res){
+/*app.get('/submitname/:name', function(req,res){
     var name = req.params.name;
     
     names.push(name);
     
     res.send(JSON.stringify(names));
     
-});
+});*/
 
 app.get('/submitname', function(req,res){
     var name = req.query.name;
