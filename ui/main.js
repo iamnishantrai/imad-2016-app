f9c72903f8f792dc90d80bdba561455fbb8c7cf7
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 20:34:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,8 @@ function onClickSubmit(){
     xhttp.onreadystatechange = function(){
         if(xhttp.readyState == XMLHttpRequest.DONE){
             if(request.status==200){
-                var names = [];
+                var names = xhhtp.responseText;
+                names = JSON.parse(names);
                 var list = '';
                 for(var i=0; i<names.length; i++){
                     list += '<li>' + names[i] + '</li>';
