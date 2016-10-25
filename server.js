fcd2561715cb037b54a344655fa8634dab49b1c8
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 20:22:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,22 +116,22 @@ return htmlTemplate;
 }
 
 
-app.get('/:articleName',function(req,res){
+/*app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
-});
+});*/
 
 
 var names = [];
 
-/*app.get('/submitname/:name', function(req,res){
+app.get('/submitname/:name', function(req,res){
     var name = req.params.name;
     
     names.push(name);
     
     res.send(JSON.stringify(names));
     
-});*/
+});
 
 app.get('/submitname', function(req,res){
     var name = req.query.name;
