8d2a34e7e1ef19440d55188b21eeef142cee96d3
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 23:46:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 
 var counter = 0;
 
-app.get('/counter',function(req,res){
+app.get('/',function(req,res){
     counter = counter+1;
     res.send(counter.toString());
 });
