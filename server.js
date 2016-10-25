3f99ecab37bfbfe18112b240af67648057cc9c4e
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 15:54:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ var articleOne = {
 function createTemplate(data){
     var title = data.title;
     var date = data.date;
-    var heeading = data.heading;
+    var heading = data.heading;
     var content = data.content;
     
     
