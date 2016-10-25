f5f31faa71b35498f70c95b6e8c143a6b7ef9366
iamnishantrai <nishant.2010rai@gmail.com>
2016-10-25 20:41:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,9 +25,6 @@ function onClick(){
 var nameInput = document.getElementById("name");
 var name = nameInput.value;
 
-var submit = document.getElementById("submit_btn");
-
-
 function onClickSubmit(){
     
     var xhttp = new XMLHttpRequest();
